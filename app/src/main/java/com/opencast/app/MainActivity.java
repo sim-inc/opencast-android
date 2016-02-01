579ffb0ba2a544b665c1730317836b35191b9e9b
Nil Késede <nil@ksde.pw>
2016-02-01 12:19:20 -0300

    Optimize imports

--- app/src/main/java/com/opencast/app/MainActivity.java
+++ app/src/main/java/com/opencast/app/MainActivity.java
@@ -2,8 +2,6 @@ package com.opencast.app;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
